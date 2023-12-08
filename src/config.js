e3a4fa8ab0dd1e40f8f7727e8420311e003bffe7
ngcxy <caixinyi2013@hotmail.com>
2023-12-08 01:19:34 -0800

    frontend host on vercel

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiUrl: 'http://localhost:4000',
-    // apiUrl: 'http://172.31.19.23/api_v1',
+    // apiUrl: 'http://localhost:4000',
+    apiUrl: 'http://35.86.87.93/api_v1',
 };
 
 exports.config = config;
