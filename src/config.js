f2e5789cdd872629a34c16c6ebc1d3b2e4ba2712
ngcxy <caixinyi2013@hotmail.com>
2023-12-12 00:49:54 -0800

    change backend url to usc-ece

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // apiUrl: 'http://localhost:4000',
-     apiUrl: 'https://ee547-proj-fa23-group2.usc-ece.com/',
+     apiUrl: 'https://ee547-proj-fa23-group2.usc-ece.com',
 };
 
 exports.config = config;
